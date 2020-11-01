2a6b8a535e89b683bed02d20f367f92a57e0061c
starychfojtu <josef.starychfojtu@gmail.com>
2020-11-01 20:38:33 +0100

    Refactoring
    Initial commit

--- BlogScraper.py
+++ BlogScraper.py
@@ -1,6 +1,13 @@
 import time
 import datetime
 import itertools
+import smtplib
+import ssl
+import sys
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 import ParsonsMatt
 
 def get_news_feed(articles, after):
@@ -9,10 +16,23 @@ def get_news_feed(articles, after):
     feed = ["<h1>News feed since {0}</h1>".format(after)]
     for blog, articles in grouped_articles:
         feed.append("<h2>{0} ({1})</h2>".format(blog.name, blog.description))
+        feed.append("<ul>")
         for article in articles:
-            feed.append('<a href="{0}">{1}</a>'.format(article.url, article.name))
+            feed.append('<li><a href="{0}">{1}</a></li>'.format(article.url, article.name))
+        feed.append("</ul>")
     
     return "\n".join(feed)
+
+def send_gmail(sender_email, password, receiver_email, subject, content):
+    message = MIMEMultipart()
+    message["Subject"] = subject
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message.attach(MIMEText(content, "html"))
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
 
 articles = list(itertools.chain.from_iterable([
     ParsonsMatt.get_articles()
@@ -20,5 +40,4 @@ articles = list(itertools.chain.from_iterable([
 
 week_ago = datetime.datetime.now() - datetime.timedelta(days=7)
 news_feed = get_news_feed(articles, after=week_ago)
-print(news_feed)
-
+send_gmail("josef.starychfojtu@gmail.com", sys.argv[0], "josef.starychfojtu@gmail.com", "Weekly news feed", news_feed)
