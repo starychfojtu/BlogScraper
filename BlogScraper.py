912baba12c3d441e4a3988e9c72bca75cbb6fab4
starychfojtu <josef.starychfojtu@gmail.com>
2020-11-03 22:44:41 +0100

    Adjustments for workshop

--- BlogScraper.py
+++ BlogScraper.py
@@ -24,17 +24,6 @@ def get_news_feed(articles, after):
     
     return "\n".join(feed)
 
-def send_gmail(sender_email, password, receiver_email, subject, content):
-    message = MIMEMultipart()
-    message["Subject"] = subject
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    message.attach(MIMEText(content, "html"))
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
-
 articles = list(itertools.chain.from_iterable([
     ParsonsMatt.get_articles(),
     TomHarding.get_articles()
@@ -42,4 +31,4 @@ articles = list(itertools.chain.from_iterable([
 
 week_ago = datetime.datetime.now() - datetime.timedelta(days=7)
 news_feed = get_news_feed(articles, after=week_ago)
-send_gmail("josef.starychfojtu@gmail.com", sys.argv[1], "josef.starychfojtu@gmail.com", "Weekly news feed", news_feed)
+print(news_feed)
--- TomHarding.py
+++ TomHarding.py
@@ -6,10 +6,10 @@ import requests
 blog = Blog("http://www.tomharding.me/", "Tom Harding", "Category/Type Theory")
 
 def parse_article(article_container):
-        title_link = article_container.find("h2", class_="post-title").find("a")
-        timestamp = date_parser.parse(article_container.find("span", class_="post-date").get_text())
-        url = blog.url + title_link.get("href")
-        return Article(blog, url, title_link.get_text(), timestamp)
+    title_link = article_container.find("h2", class_="post-title").find("a")
+    timestamp = date_parser.parse(article_container.find("span", class_="post-date").get_text())
+    url = blog.url + title_link.get("href")
+    return Article(blog, url, title_link.get_text(), timestamp)
 
 def get_articles():
     page = requests.get(blog.url)
