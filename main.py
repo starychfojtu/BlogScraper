ffec4aec1bd0f694ce46dac0537230a01631cc32
starychfojtu <josef.starychfojtu@gmail.com>
2020-11-03 23:39:27 +0100

    Mailgun configured

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import datetime
 import itertools
-import smtplib
-import ssl
-
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+import requests
 
 import ParsonsMatt
 import TomHarding
@@ -22,16 +18,17 @@ def get_news_feed(articles, after):
     
     return "\n".join(feed)
 
-def send_gmail(sender_email, password, receiver_email, subject, content):
-    message = MIMEMultipart()
-    message["Subject"] = subject
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    message.attach(MIMEText(content, "html"))
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
+def send_email(content):
+	return requests.post(
+		"https://api.mailgun.net/v3/sandboxb243fe7e7f9d437099b9d7aa678f0b7f.mailgun.org/messages",
+		auth=("api", "API_KEY"),
+		data={
+            "from": "Mailgun Sandbox <postmaster@sandboxb243fe7e7f9d437099b9d7aa678f0b7f.mailgun.org>",
+			"to": "email@gmail.com",
+			"subject": "Weekly news feed",
+			"html": content
+        }
+    )
 
 articles = list(itertools.chain.from_iterable([
     ParsonsMatt.get_articles(),
@@ -42,4 +39,4 @@ week_ago = datetime.datetime.now() - datetime.timedelta(days=7)
 news_feed = get_news_feed(articles, after=week_ago)
 print(news_feed)
 
-send_gmail("your_mail@gmail.com", "pass_here", "receiver_mail@gmail.com", "Weekly news feed", news_feed)
+print(send_email(news_feed).content)
