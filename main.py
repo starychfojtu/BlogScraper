975c158e17905dfa7e8bfdb71f1a2947d58ff90b
starychfojtu <josef.starychfojtu@gmail.com>
2020-11-03 23:21:04 +0100

    mail returned

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import datetime
 import itertools
+import smtplib
+import ssl
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 import ParsonsMatt
 import TomHarding
@@ -17,6 +22,17 @@ def get_news_feed(articles, after):
     
     return "\n".join(feed)
 
+def send_gmail(sender_email, password, receiver_email, subject, content):
+    message = MIMEMultipart()
+    message["Subject"] = subject
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message.attach(MIMEText(content, "html"))
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
 articles = list(itertools.chain.from_iterable([
     ParsonsMatt.get_articles(),
     TomHarding.get_articles()
@@ -25,3 +41,5 @@ articles = list(itertools.chain.from_iterable([
 week_ago = datetime.datetime.now() - datetime.timedelta(days=7)
 news_feed = get_news_feed(articles, after=week_ago)
 print(news_feed)
+
+send_gmail("your_mail@gmail.com", "pass_here", "receiver_mail@gmail.com", "Weekly news feed", news_feed)
